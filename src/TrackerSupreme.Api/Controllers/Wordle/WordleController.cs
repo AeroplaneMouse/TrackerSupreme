f5e48816d7d5562ba9b75bb590c8f57de8e6b2c0
Daniel Fly <danielfly42@gmail.com>
2025-11-13 23:45:08 +0100

    Initial solution

---
+++ src/TrackerSupreme.Api/Controllers/Wordle/WordleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackerSupreme.Api.Controllers.Wordle
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordleController : ControllerBase
+    {
+    }
+}
